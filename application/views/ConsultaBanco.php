5ca01c62a3a247c824c0d0bf23ee87b5919478d5
gianfranco9261 <thegodfather9261@outlook.es>
2017-12-10 21:23:44 -0500

    21231

--- application/views/ConsultaBanco.php
+++ application/views/ConsultaBanco.php
@@ -7,7 +7,7 @@
     <script type="text/javascript" src="<?php echo base_url()?>public/jquery/jquery-3.1.1.min.js"></script>
     <script  type="text/javascript"  src="<?php echo base_url()?>public/bootstrap-3.3.7-dist/js/bootstrap.min.js"> </script>
 <!-- quiero jugar un imba 2-->
-    <!-- hola george 2-->
+    <!-- hola kevin-->
      <style type="text/css">
 		.modal-static { 
 		    position: fixed;
